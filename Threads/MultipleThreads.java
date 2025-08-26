0a92102978c06162f6918618af43d3fe0efbeaad
THUMMALA CHAKRAVARTHI <thummalachakravarthi2002@gmail.com>
2025-08-26 07:48:03 +0530

    adding multiplethreads.java

--- Threads/MultipleThreads.java
+++ Threads/MultipleThreads.java
@@ -1,1 +1,29 @@
+package com.advancedjava;
+
+
+class Aaa extends Thread{
+	public void run() {
+		for(int i=1;i<=100;i++){
+		System.out.println("hi");
+		}
+	}
+}
+
+class Baa extends Thread{
+	public void run() {
+		for(int i=1;i<=10;i++) {
+		System.out.println("hello");
+		}
+	}
+}
+
+public class threads{
+ public static void main(String[] args) {
+	 Aaa obj=new Aaa();
+	 Baa obj1=new Baa();
+	 obj.start();
+	 obj1.start();
+	 
+ }
+}
 
