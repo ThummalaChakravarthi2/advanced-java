687cd8fb2abeacb271979c09ed9d880bf31d5f36
THUMMALA CHAKRAVARTHI <thummalachakravarthi2002@gmail.com>
2025-08-27 17:56:39 +0530

    using collections without type

---
+++ Collections/ArrayList.java
@@ -0,0 +1,20 @@
+package collections;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ArrayLists{
+	public static void main(String[] args) {
+    // without using type
+		Collection nums=new ArrayList();
+		nums.add(2);
+		nums.add(4);
+		nums.add(3);
+		nums.add(6);
+		nums.add(8);
+		nums.add(9);
+		System.out.println(nums);
+		
+ 
+	}
+
+}
