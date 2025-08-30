7c30bd2f356c3d093605a54b00f5d18111106477
THUMMALA CHAKRAVARTHI <thummalachakravarthi2002@gmail.com>
2025-08-31 00:00:23 +0530

    MapKeywordInStream its actual implementation

---
+++ Collections/MapWithFunction.java
@@ -0,0 +1,32 @@
+package collections;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class FilterPredicate {
+
+	public static void main(String[] args) {
+		List<Integer> nums=Arrays.asList(2,3,4,5,6);
+		Function<Integer,Integer> fun=new Function<>() {
+
+			@Override
+			public Integer apply(Integer t) {
+				
+				return t*2;
+			}
+			
+		};
+	   int result=nums.stream()
+	                  .filter(n->n%2==0)
+	                  .map(fun)
+	                  .reduce(0,(c,e)->c+e);
+         System.out.println(result);
+		    
+
+	}
+
+}
